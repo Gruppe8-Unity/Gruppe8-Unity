6b2749c64dac8a9608755369873cbd11778ad5b1
Gruppe 8 - Unity <253802@usn.no>
2024-05-21 16:46:48 +0200

    1.Spill bevegelse
    Spilleren kan nå beveges ved å bruke piltaster på tastaturet.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,5 +8,43 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public float movementSpeed = 10.0f;
+    public Transform playerTransform;
 
+    private void Start()
+    {
+        // Get transform of associated gameobject
+        playerTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        float verticalMovement = 0.0f;
+        float horizontalMovement = 0.0f;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            verticalMovement = 1.0f;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            verticalMovement = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalMovement = 1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontalMovement = -1.0f;
+        }
+
+        // Scaled movement in x and y directions
+        float movementX = movementSpeed * horizontalMovement * Time.deltaTime;
+        float movementY = movementSpeed * verticalMovement * Time.deltaTime;
+        Vector2 movementDirection = new Vector2(movementX, movementY);
+        playerTransform.Translate(movementDirection);
+    }
 }
