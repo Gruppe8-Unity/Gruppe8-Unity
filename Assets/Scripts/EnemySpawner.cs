f1b05f31cfbccfafafb407decfb68021f57b6d83
Gruppe 8 - Unity <253802@usn.no>
2024-05-21 16:54:17 +0200

    3a. Fiender
    Lagt til fiender som spawner og som beveger seg mot spillerobjektet.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,10 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
 
-    
+    [SerializeField]
+    private float _minimumSpawnTime;
+
+    [SerializeField]
+    private float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = UnityEngine.Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+    private void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    private void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0)
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
 }
