037b983b3f27350b0a6f4fac263152a989912aa1
Gruppe-8-Unity <253802@usn.no>
2024-05-23 19:36:44 +0200

    Fikset problemer som oppstod ved merge.

--- Assets/Scripts/WeaponAxe.cs
+++ Assets/Scripts/WeaponAxe.cs
@@ -7,13 +7,21 @@ public class WeaponAxe : MonoBehaviour
     public float speed = 20f;
     public Rigidbody2D axeRigidBody;
     public Vector2 throwDirection = new Vector2(0.2f, 1f);
+    public float axeLifeTime = 5f;
     void Start()
     {
         Vector2 normalizedDirection = throwDirection.normalized;
         axeRigidBody.velocity = normalizedDirection * speed;
     }
-    
 
+    void Update()
+    {
+        if (axeLifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
 
+        axeLifeTime -= Time.deltaTime;
+    }
     
 }
